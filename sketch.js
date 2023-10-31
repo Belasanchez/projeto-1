1562fec9c19c0cf84e55c4c66deafcdf121c342b
Belasanchez <148897121+Belasanchez@users.noreply.github.com>
2023-10-31 13:52:00 -0300

    adicionando os arquivos html.js e css

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(400, 400);
+    background ("pink");
+  }
+  
+  function draw() {
+    stroke("white")
+  fill ("cyan")
+  { 
+    if(mouseIsPressed)
+  rect (mouseX,mouseY, 20,20);
+  }
+  }
